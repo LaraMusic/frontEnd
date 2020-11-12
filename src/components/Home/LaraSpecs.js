2cf002212dcb1955664441a6ea520b6dd43820af
Jobana Garavito Q <nellyjobana@gmail.com>
2020-11-12 13:26:10 -0500

    changing dummy text

--- src/components/Home/LaraSpecs.js
+++ src/components/Home/LaraSpecs.js
@@ -4,33 +4,32 @@ import FeatureImg from "../../../assets/img/hero-cell.png";
 import HomeStyle from "../Style/Home/HomeStyle";
 
 function LaraSpecs() {
-  return (
-    <section className="lara__specs">
-      <div className="container">
-        <div className="lara__specs__description">
-          <div className="description__section">
-            <h1 className="description__title">
-              Listening and watching anytime, anywhere
-            </h1>
-            <p className="description__p">
-              The artists we represent are one of the most successful in Romania
-              and also were a huge breakthrough.
-            </p>
-          </div>
-          <div className="lara__specs__description__cards">
-            <CardLaraSpec></CardLaraSpec>
-          </div>
-        </div>
-        <div className="lara__specs__img">
-          <img src={FeatureImg} alt="lara-features" />
-          <p></p>
-        </div>
-      </div>
+	return (
+		<section className='lara__specs'>
+			<div className='container'>
+				<div className='lara__specs__description'>
+					<div className='description__section'>
+						<h1 className='description__title'>
+							Listen your favorites artists anytime, anywhere
+						</h1>
+						<p className='description__p'>
+							Play your favorite music and discover new music everyday.
+						</p>
+					</div>
+					<div className='lara__specs__description__cards'>
+						<CardLaraSpec></CardLaraSpec>
+					</div>
+				</div>
+				<div className='lara__specs__img'>
+					<img src={FeatureImg} alt='lara-features' />
+					<p></p>
+				</div>
+			</div>
 
-      <style jsx HomeStyle>
-        {HomeStyle}
-      </style>
-    </section>
-  );
+			<style jsx HomeStyle>
+				{HomeStyle}
+			</style>
+		</section>
+	);
 }
 export default LaraSpecs;
--- src/components/Home/TopArtist.js
+++ src/components/Home/TopArtist.js
@@ -3,27 +3,24 @@ import CardArtist from "./molecules/CardArtist";
 import HomeStyle from "../Style/Home/HomeStyle";
 
 function TopArtist() {
-  return (
-    <section className="artist">
-      <div className="container">
-        <div className="artist__info">
-          <h1>New artists</h1>
-          <p>
-            The artists we represent are one of the most successful in Romania
-            and also were a huge breakthrough.
-          </p>
-        </div>
-        <div className="container__grid">
-          <div className="grid__container__artist">
-            <CardArtist></CardArtist>
-          </div>
-        </div>
-      </div>
+	return (
+		<section className='artist'>
+			<div className='container'>
+				<div className='artist__info'>
+					<h1>Top artists</h1>
+					<p>The most popular today.</p>
+				</div>
+				<div className='container__grid'>
+					<div className='grid__container__artist'>
+						<CardArtist></CardArtist>
+					</div>
+				</div>
+			</div>
 
-      <style jsx HomeStyle>
-        {HomeStyle}
-      </style>
-    </section>
-  );
+			<style jsx HomeStyle>
+				{HomeStyle}
+			</style>
+		</section>
+	);
 }
 export default TopArtist;
