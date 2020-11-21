857f7237fa127c1c561d0d3934a70f47c68241d1
fercc1097 <fercc1097@gmail.com>
2020-11-21 14:23:28 -0600

    last changes made

--- src/components/Player/ListPlayer.js
+++ src/components/Player/ListPlayer.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { useAuth } from "../../contexts/AuthContext";
-import SectionPopular from "./molecules/SectionPopular.js";
-import FeaturedTracks from "./FeaturedTracks";
+import React from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import SectionPopular from './molecules/SectionPopular.js';
+import FeaturedTracks from './FeaturedTracks';
 
-import RoundPlayButton from "../Play/molecules/RoundPlayButton";
-import { VscThumbsup } from "react-icons/vsc";
-import { GrAddCircle } from "react-icons/gr";
-import logoAlbum from "../../../assets/img/album.png";
+import RoundPlayButton from '../Play/molecules/RoundPlayButton';
+import { VscThumbsup } from 'react-icons/vsc';
+import { GrAddCircle } from 'react-icons/gr';
+import logoAlbum from '../../../assets/img/album.png';
 
-import FavoritesStyles from "../Style/Favorites/FavoritesStyles";
+import FavoritesStyles from '../Style/Favorites/FavoritesStyles';
 
 const List = ({ title }) => {
   const { user } = useAuth();
@@ -19,37 +19,41 @@ const List = ({ title }) => {
     musiclist.find((playlist) => playlist.title === title)?.musictracks ?? [];
 
   return (
-    <main className="containerContent">
+    <main className='containerContent'>
       <FeaturedTracks title={title} />
-      <section className="about__album">
-        <div className="banner__song">
-          <img src={logoAlbum} alt="list-image" />
+      <section className='about__album'>
+        <div className='banner__song'>
+          <img src={logoAlbum} alt='list-image' />
         </div>
-        <div className="info__album_container">
-          <a href="">// My personal list</a>
-          <h1 className="title__album">{title}</h1>
-          <h2 className="authors__album">
+        <div className='info__album_container'>
+          <a href=''>// My personal list</a>
+          <h1 className='title__album'>{title}</h1>
+          <h2 className='authors__album'>
             Build a unlimited playlist, and listen every time
           </h2>
 
-          <section className="content__music">
-            {tracks.map((item) => (
-              <div className="tab__music" key={item.id}>
-                <div className="tab__music__icons">
-                  <div className="icon">
-                    <RoundPlayButton song={item} />
+          <section className='content__music'>
+            {tracks.map((item) => {
+              console.log(item);
+
+              return (
+                <div className='tab__music' key={item.id}>
+                  <div className='tab__music__icons'>
+                    <div className='icon'>
+                      <RoundPlayButton song={item} />
+                    </div>
                   </div>
+                  <p className='tab__music__title'>
+                    {item.title.substring(0, 10)}
+                  </p>
+                  <p className='tab__music__plays'>
+                    {item.artist.substring(0, 3)}
+                  </p>
+                  <p className='tab__music__time'>{item.fuente}</p>
+                  <p className='tab__music__time'>{item.gender}</p>
                 </div>
-                <p className="tab__music__title">
-                  {item.title.substring(0, 10)}
-                </p>
-                <p className="tab__music__plays">
-                  {item.artist.substring(0, 3)}
-                </p>
-                <p className="tab__music__time">{item.fuente}</p>
-                <p className="tab__music__time">{item.gender}</p>
-              </div>
-            ))}
+              );
+            })}
           </section>
           {/* <div className='show__more'>
 						<p>Show More</p>
